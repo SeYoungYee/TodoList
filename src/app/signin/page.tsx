ce48d2c5fec6ce43365b4509bf2dfaca4c9f3018
DESKTOP-SUURT89\SeYoung <parksy4073@naver.com>
2025-09-02 23:37:22 +0900

    로그인된 todo 데이터 불러오기

--- src/app/signin/page.tsx
+++ src/app/signin/page.tsx
@@ -12,7 +12,6 @@ export default function LoginPage() {
 
   const handleLogin = async (e: React.FormEvent) => {
     e.preventDefault();
-
     const { data, error } = await supabase.auth.signInWithPassword({
       email,
       password,
